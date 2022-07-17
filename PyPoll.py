f5b03314b83030de6400fcb457f6428065613663
Moataz Yaakoub <moatazyaakoub@Moatazs-MacBook-Pro-2.local>
2022-07-16 23:02:36 -0400

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,54 @@
+#The data we need to retrieve.
+#1. - The total number•of votes cast
+#2. A complete list of candidates-who-received-votes
+#3. The percentage of votes each • candidate won
+#4. The-total number of votes each candidate won
+#5.- The winner of•the election based on popular vote.
+
+# import csv
+# import os
+# # Assign a variable for the file to load and the path.
+# file_to_load = os.path.join("Resources", "election_results.csv")
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+
+#     # Print the file object.
+#      print(election_data)
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Use the open statement to open the file as a text file.
+# outfile = open(file_to_save, "w")
+# # Write some data to the file.
+# outfile.write("Hello World")
+
+# # Close the file
+# outfile.close()
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+
+#     # Write some data to the file.
+#             txt_file.write("Countries in the Election\n------------------\nArapahoe\nDenver\nJefferson")
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
